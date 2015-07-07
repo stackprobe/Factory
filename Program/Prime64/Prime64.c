cbf44591bea3fb03b525dd3bf6e0b6d4d9875fe4
stackprobe <stackprobe@gmail.com>
2015-07-08 05:41:16 +0900

    b

--- Program/Prime64/Prime64.c
+++ Program/Prime64/Prime64.c
@@ -73,7 +73,7 @@ static void PrimeRange(uint64 minval, uint64 maxval, char *outFile, char *cancel
 	uint cancelEv = eventOpen(cancelEvName);
 	uint reportEv = eventOpen(reportEvName);
 	uint reportMtx = mutexOpen(reportMtxName);
-	uint64 count;
+	uint64 value;
 
 //	errorCase(setvbuf(fp, NULL, _IOFBF, 64 * 1024 * 1024)); // ? Ž¸”s
 
@@ -89,28 +89,82 @@ static void PrimeRange(uint64 minval, uint64 maxval, char *outFile, char *cancel
 
 	m_minim(maxval, PRIME_MAX);
 
-	for(count = minval; count <= maxval; count += 2)
+	for(value = minval; value <= maxval; value += 2)
 	{
-		if(count % 0x08000000 == 1)
+		if(value % 0x08000000 == 1)
 		{
 			if(handleWaitForMillis(cancelEv, 0))
 			{
+				WrUI64Flush(fp);
 				errorCase(fprintf(fp, "’†Ž~‚µ‚Ü‚µ‚½B\n") < 0);
 				break;
 			}
 
 			handleWaitForever(reportMtx);
-			writeOneLine_cx(reportFile, xcout("%I64u\n", count));
+			writeOneLine_cx(reportFile, xcout("%I64u", value));
 			mutexRelease(reportMtx);
 
 			eventSet(reportEv);
 		}
-		if(IsPrime_R(count))
-//			errorCase(fprintf(fp, "%I64u\n", count) < 0);
-			WrUI64(fp, count);
+		if(IsPrime_R(value))
+//			errorCase(fprintf(fp, "%I64u\n", value) < 0);
+			WrUI64(fp, value);
 	}
 	WrUI64Flush(fp);
 	fileClose(fp);
+
+	handleWaitForever(reportMtx);
+	removeFileIfExist(reportFile);
+	mutexRelease(reportMtx);
+
+	handleClose(cancelEv);
+	handleClose(reportEv);
+	handleClose(reportMtx);
+}
+static void PrimeCount(uint64 minval, uint64 maxval, char *outFile, char *cancelEvName, char *reportEvName, char *reportMtxName, char *reportFile)
+{
+	uint cancelEv = eventOpen(cancelEvName);
+	uint reportEv = eventOpen(reportEvName);
+	uint reportMtx = mutexOpen(reportMtxName);
+	uint64 value;
+	uint64 count = 0;
+	int cancelled = 0;
+
+	removeFileIfExist(outFile);
+
+	if(minval <= 2)
+	{
+		if(2 <= maxval)
+			count++;
+
+		minval = 3;
+	}
+	else
+		minval |= 1;
+
+	m_minim(maxval, PRIME_MAX);
+
+	for(value = minval; value <= maxval; value += 2)
+	{
+		if(value % 0x10000000 == 1)
+		{
+			if(handleWaitForMillis(cancelEv, 0))
+			{
+				cancelled = 1;
+				break;
+			}
+
+			handleWaitForever(reportMtx);
+			writeOneLine_cx(reportFile, xcout("%I64u\n%I64u", value, count));
+			mutexRelease(reportMtx);
+
+			eventSet(reportEv);
+		}
+		if(IsPrime_R(value))
+			count++;
+	}
+	if(!cancelled)
+		writeOneLine_cx(outFile, xcout("%I64u", count));
 
 	handleWaitForever(reportMtx);
 	removeFileIfExist(reportFile);
@@ -227,9 +281,19 @@ static void Main2(void)
 		else
 			outFile = NULL;
 
+		if(minval <= 2)
+		{
+			if(2 <= maxval)
+				count++;
+
+			minval = 3;
+		}
+		else
+			minval |= 1;
+
 		m_minim(maxval, PRIME_MAX);
 
-		for(value = minval; value <= maxval; value++)
+		for(value = minval; value <= maxval; value += 2)
 			if(IsPrime_R(value))
 				count++;
 
@@ -238,6 +302,27 @@ static void Main2(void)
 		if(outFile)
 			writeOneLine_cx(outFile, xcout("%I64u", count));
 
+		return;
+	}
+	if(argIs("/C2"))
+	{
+		uint64 minval;
+		uint64 maxval;
+		char *outFile;
+		char *cancelEvName;
+		char *reportEvName;
+		char *reportMtxName;
+		char *reportFile;
+
+		minval = toValue64(nextArg());
+		maxval = toValue64(nextArg());
+		outFile = nextArg();
+		cancelEvName = nextArg();
+		reportEvName = nextArg();
+		reportMtxName = nextArg();
+		reportFile = nextArg();
+
+		PrimeCount(minval, maxval, outFile, cancelEvName, reportEvName, reportMtxName, reportFile);
 		return;
 	}
 }
