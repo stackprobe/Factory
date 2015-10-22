4da3b7959c86679f9170259de3c487a39fb2b2ac
stackprobe <stackprobe@gmail.com>
2015-10-22 14:44:05 +0900

    c

--- Labo/Fx/libs/FxTime.c
+++ Labo/Fx/libs/FxTime.c
@@ -6,13 +6,13 @@
 
 #define START_TIME (0 * 86400 + 7 * 3600 +  0 * 60) // 月曜日(0日後) 07:00:00
 #define END_TIME   (5 * 86400 + 5 * 3600 + 50 * 60) // 土曜日(5日後) 05:50:00
-#define TIME_CYCLE (7 * 86400) // 1週間
+#define TIME_CYCLE (7 * 86400) // １週間
 
 #define TRADING_TIME (END_TIME - START_TIME) // 取引可能期間
 #define INTERVAL_TIME (TIME_CYCLE - TRADING_TIME) // お休み期間
 
 /*
-	sec が、お休み期間の場合、直後の取引可能期間の最初の時間
+	ret: sec が、お休み期間の場合、直後の取引可能期間の最初の時間
 */
 uint64 Sec2FxTime(uint64 sec)
 {
