1fee2457dfce71b7b7bc4b47c5a6527b2f683dfb
stackprobe <stackprobe@gmail.com>
2016-04-08 14:14:15 +0900

    b

--- Labo/utest/auto/Common/Line.c
+++ Labo/utest/auto/Common/Line.c
@@ -1,4 +1,5 @@
 #include "C:\Factory\Common\all.h"
+#include "C:\Factory\DevTools\libs\RandData.h"
 
 static void DoTest_copyBlock(char *beforeData, uint destPos, uint srcPos, uint size, char *afterData)
 {
@@ -24,8 +25,62 @@ static void Test_copyBlock(void)
 
 	cout("OK\n");
 }
+static char *TestFunc_trimSequ(char *str)
+{
+	error(); // TODO
+	return NULL;
+}
+static void DoTest_trimSequ(char *str, char *ext)
+{
+	uint strsz = strlen(str) + 1;
+	uint extsz = strlen(ext) + 1;
+	uint size;
+	char *buff1;
+	char *buff2;
 
+	size = strsz + extsz;
+
+	buff1 = memAlloc(size);
+	buff2 = memAlloc(size);
+
+	memcpy(buff1,         str, strsz);
+	memcpy(buff1 + strsz, ext, extsz);
+
+	str = TestFunc_trimSequ(str);
+
+	memcpy(buff2,         str, strsz);
+	memcpy(buff2 + strsz, ext, extsz);
+
+	trimSequ(buff1, ' ');
+
+	errorCase(strcmp(buff1, buff2));
+	errorCase(strcmp(buff1 + strsz, buff2 + strsz));
+
+	memFree(buff1);
+	memFree(buff2);
+}
+static void Test_trimSequ(void)
+{
+	uint c;
+
+	mt19937_init32((uint)time(NULL));
+
+	MRLChrLow = 0x20;
+	MRLChrLow = 0x21;
+
+	for(c = 0; c < 1000; c++)
+	{
+		char *str = MakeRandLineRange(0, 1000);
+		char *ext = MakeRandLineRange(0, 1000);
+
+		DoTest_trimSequ(str, ext);
+
+		memFree(str);
+		memFree(ext);
+	}
+}
 int main(int argc, char **argv)
 {
 	Test_copyBlock();
+	Test_trimSequ();
 }
