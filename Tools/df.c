6b186d543970e596195197775c201e07032c8329
stackprobe <stackprobe@gmail.com>
2016-06-22 15:14:38 +0900

    b

--- Tools/df.c
+++ Tools/df.c
@@ -1,12 +1,18 @@
 #include "C:\Factory\Common\all.h"
+#include "C:\Factory\Common\Options\Calc2.h"
 
+static char *GetPct(uint64 numer, uint64 denom)
+{
+	calcBasement = 2;
+	return calc_xx(xcout("%I64u00", numer), '/', xcout("%I64u", denom));
+}
 static void DispDiskFree(int drive)
 {
 	updateDiskSpace(drive);
 
 	cout("%cドライブ\n", m_toupper(drive));
 	cout("ディスク使用率 = %.3f %%\n", 100.0 - lastDiskFree / (double)lastDiskSize * 100.0);
-	cout("ディスク空き = %I64u バイト (使用可能 = %I64u バイト)\n", lastDiskFree, lastDiskFree_User);
+	cout("ディスク空き = %I64u バイト (使用可能 = %I64u バイト, %s %%)\n", lastDiskFree, lastDiskFree_User, GetPct(lastDiskFree_User, lastDiskFree));
 	cout("ディスク容量 = %I64u バイト\n", lastDiskSize);
 }
 int main(int argc, char **argv)
