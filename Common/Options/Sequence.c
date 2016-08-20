2e8c1189601682509570e77d4d07fa2756dcce93
stackprobe <stackprobe@gmail.com>
2016-08-20 22:26:50 +0900

    d

--- Common/Options/Sequence.c
+++ Common/Options/Sequence.c
@@ -30,23 +30,43 @@ void sortSq(autoList_t *list)
 {
 	rapidSort(list, simpleComp);
 }
-uint chooseOneSq(autoList_t *list, uint target)
+uint chooseOneSq(autoList_t *list, int (*isTarget)(uint))
 {
+#if 1
 	uint count = 0;
 	uint value;
 	uint index;
 
 	foreach(list, value, index)
-		if(value == target)
+		if(isTarget(value))
 			count++;
 
 	count = mt19937_rnd(count);
 
 	foreach(list, value, index)
-		if(value == target)
-			goto endFunc;
+	{
+		if(isTarget(value))
+		{
+			if(!count)
+				goto foundChoseTarget;
 
+			count--;
+		}
+	}
 	error();
-endFunc:
+foundChoseTarget:
 	return index;
+#else
+	autoList_t *indexes = newList();
+	uint value;
+	uint index;
+
+	foreach(list, value, index)
+		if(isTarget(value))
+			addElement(indexes, index);
+
+	index = getElement(indexes, mt19937_rnd(getCount(indexes)));
+	releaseAutoList(indexes);
+	return index;
+#endif
 }
--- Common/Options/Sequence.h
+++ Common/Options/Sequence.h
@@ -7,4 +7,4 @@ void makeSq(autoList_t *list, uint start, sint step);
 autoList_t *createSq(uint count, uint start, sint step);
 void shuffle(autoList_t *list);
 void sortSq(autoList_t *list);
-uint chooseOneSq(autoList_t *list, uint target);
+uint chooseOneSq(autoList_t *list, int (*isTarget)(uint));
