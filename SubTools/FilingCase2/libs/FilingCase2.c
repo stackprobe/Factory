82da8819191b31b91d26a5ecef2801202118ebed
stackprobe <stackprobe@gmail.com>
2016-09-05 10:08:05 +0900

    b

---
+++ SubTools/FilingCase2/libs/FilingCase2.c
@@ -0,0 +1,18 @@
+/*
+	'U', 'u' ‚ÅŽn‚Ü‚é K ‚Í unq + direct
+
+	uuid "_m" ‚Å mtx
+
+	[tablehash]/
+		record/
+			00/00/00/00/[rec-hash] = { K, V, K, V, ... }
+
+		direct/
+			00/00/00/00/[key-hash]/
+			00/00/00/00/[val-hash]/[rec-hash] = { }
+
+	filingCase2.id = { uuid }
+	table = { T, T, T, ... }
+*/
+
+#include "FilingCase2.h"
