a7696272bbed3f7a909498d7eb9f8ad7ac413438
stackprobe <stackprobe@gmail.com>
2016-10-17 22:29:11 +0900

    d

--- Common/System.c
+++ Common/System.c
@@ -126,12 +126,13 @@ uint64 nowTick(void)
 
 		if(UINTMAX / 2 < diffTick) // オーバーフローだろう。
 		{
-			LOGPOS();
+			cout("GetTickCount overflow\n");
+//			LOGPOS(); // 再帰しとるやんけプーw @ 2016.10.17
 			baseTick += (uint64)UINT_MAX + 1;
 		}
 		else // オーバーフローか？
 		{
-			LOGPOS();
+//			LOGPOS(); // 再帰しとるやんけプーw @ 2016.10.17
 			baseTick += diffTick; // 前回と同じ戻り値になるように調整する。
 		}
 	}
