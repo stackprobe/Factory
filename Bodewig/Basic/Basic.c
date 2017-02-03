3114e0bac7eecaa67179e94f7005cacf7a8ea542
stackprobe <stackprobes@gmail.com>
2017-02-03 22:09:31 +0900

    z

---
+++ Bodewig/Basic/Basic.c
@@ -0,0 +1,5 @@
+#include "C:\Factory\Bodewig\Basic\libs\all.h"
+
+int main(int argc, char **argv)
+{
+}
--- Bodewig/T89Basic/Basic.c
+++
@@ -1,5 +0,0 @@
-#include "C:\Factory\Bodewig\T89Basic\libs\all.h"
-
-int main(int argc, char **argv)
-{
-}
