ce72bcab702e20e1713b4ce7649a2133be23a841
stackprobe <stackprobes@gmail.com>
2017-04-18 17:47:07 +0900

    a

--- Common/Options/CryptoRand_MS.c
+++ Common/Options/CryptoRand_MS.c
@@ -5,8 +5,10 @@ int createKeyContainer(void) // ret: ? 成功
 	HCRYPTPROV hp;
 
 	if(!CryptAcquireContext(&hp, 0, 0, PROV_RSA_FULL, CRYPT_NEWKEYSET)) // ? キーコンテナ作成失敗
+	{
+		cout("Failed to create key container. Last error: %08x\n", GetLastError());
 		return 0;
-
+	}
 	CryptReleaseContext(hp, 0);
 	return 1;
 }
@@ -15,10 +17,11 @@ int deleteKeyContainer(void) // ret: ? 成功
 	HCRYPTPROV hp;
 
 	if(!CryptAcquireContext(&hp, 0, 0, PROV_RSA_FULL, CRYPT_DELETEKEYSET)) // ? キーコンテナ削除失敗
+	{
+		cout("Failed to delete key container. Last error: %08x\n", GetLastError());
 		return 0;
-
-	// hp 閉じるの不要らしい。
-
+	}
+	// hp 閉じなくて良いらしい。まぁ削除だし..
 	return 1;
 }
 void getCryptoBlock_MS(uchar *buffer, uint size)
@@ -27,14 +30,22 @@ void getCryptoBlock_MS(uchar *buffer, uint size)
 
 	cout("Read sequence of %u bytes from 'CryptGenRandom' function.\n", size);
 
+#if 1
+	if(!CryptAcquireContext(&hp, 0, 0, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT))
+	{
+		cout("Failed to open CSP. Last error: %08x\n", GetLastError());
+		error();
+	}
+#else
 	if(!CryptAcquireContext(&hp, 0, 0, PROV_RSA_FULL, 0) &&
 		(GetLastError() != NTE_BAD_KEYSET ||
 			(cout("Create key container.\n"),
 			!CryptAcquireContext(&hp, 0, 0, PROV_RSA_FULL, CRYPT_NEWKEYSET))))
 	{
-		cout("Last error: %08x\n", GetLastError());
+		cout("Failed to create/open key container. Last error: %08x\n", GetLastError());
 		error();
 	}
+#endif
 
 	if(!CryptGenRandom(hp, size, buffer))
 	{
