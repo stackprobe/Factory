47356ec83a961b3a3c05944c99c59c16f8005937
stackprobe <stackprobes@gmail.com>
2017-06-13 08:39:42 +0900

    b

--- Labo/utest/unauto/Tools/Cluster.c
+++ Labo/utest/unauto/Tools/Cluster.c
@@ -2,9 +2,58 @@
 
 #define CLUSTER_EXE "C:\\Factory\\Tools\\Cluster.exe"
 
+static uint64 GetI64Stamp(time_t t)
+{
+	return toValue64_x(makeCompactStamp(getStampDataTime(t)));
+}
 static void MakeReportFile(char *dir, char *repFile)
 {
-	error(); // TODO
+	autoList_t *report = newList();
+	autoList_t *paths;
+	char *path;
+	uint index;
+
+	// 2bs
+	errorCase(lsDirAction);
+	errorCase(lsFileAction);
+	errorCase(lsInfos);
+
+	dir = makeFullPath(dir);
+	paths = lss(dir);
+	sortJLinesICase(paths);
+
+	foreach(paths, path, index)
+	{
+		char *line;
+
+		updateFindData(path);
+
+		line = xcout(
+			"%c%c%c%c%c "
+			,lastFindData.attrib & _A_ARCH   ? 'A' : ' '
+			,lastFindData.attrib & _A_HIDDEN ? 'H' : ' '
+			,lastFindData.attrib & _A_RDONLY ? 'R' : ' '
+			,lastFindData.attrib & _A_SUBDIR ? 'D' : ' '
+			,lastFindData.attrib & _A_SYSTEM ? 'S' : ' '
+			);
+
+		if(existFile(path))
+			line = addLine_x(line, xcout(
+				"%I64d %I64d %I64d "
+				,GetI64Stamp(lastFindData.time_create)
+				,GetI64Stamp(lastFindData.time_access)
+				,GetI64Stamp(lastFindData.time_write)
+				));
+
+		line = addLine(line, eraseRoot(path, dir));
+
+		addElement(report, (uint)line);
+	}
+	writeLines(repFile, report);
+
+	memFree(dir);
+	releaseDim(paths, 1);
+	releaseDim(report, 1);
 }
 static void MakeClusterI(char *rDir, char *wFile)
 {
@@ -16,10 +65,14 @@ static void UnmakeClusterI(char *rFile, char *wDir)
 }
 static void ClusterWithInfoTest(char *dir)
 {
-	char *repFile1 = makeTempPath("ClusterWithInfo-Test-Report-1.txt");
-	char *repFile2 = makeTempPath("ClusterWithInfo-Test-Report-2.txt");
+	char *repFileBase = makeTempPath(NULL);
+	char *repFile1;
+	char *repFile2;
 	char *mFile = makeTempPath(NULL);
 	char *wDir = makeTempPath(NULL);
+
+	repFile1 = xcout("%s.ClusterWithInfo-Test-Report-1.txt", repFileBase);
+	repFile2 = xcout("%s.ClusterWithInfo-Test-Report-2.txt", repFileBase);
 
 	MakeReportFile(dir, repFile1);
 
@@ -32,6 +85,7 @@ static void ClusterWithInfoTest(char *dir)
 
 	// ëSïîçÌèúÇµÇ»Ç¢ÅB
 
+	memFree(repFileBase);
 	memFree(repFile1);
 	memFree(repFile2);
 	memFree(mFile);
