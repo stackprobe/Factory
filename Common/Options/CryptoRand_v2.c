5804b204a8d108429ffcdc13f4c5911ee5acee8c
stackprobe <stackprobes@gmail.com>
2017-11-29 09:07:08 +0900

    daily

--- Common/Options/CryptoRand_v2.c
+++ Common/Options/CryptoRand_v2.c
@@ -1,7 +1,7 @@
 /*
 	getCryptoByte()のバイト列:
 
-		xor(a1, a2)
+		xor(a1, a2, a3, a4)
 
 	a1:
 		Camellia(b[0], k1) +
@@ -10,9 +10,21 @@
 		...
 
 	a2:
-		Camellia(b[0], k2) +
-		Camellia(b[1], k2) +
-		Camellia(b[2], k2) +
+		Camellia(b[2^126 + 0], k2) +
+		Camellia(b[2^126 + 1], k2) +
+		Camellia(b[2^126 + 2], k2) +
+		...
+
+	a3:
+		Camellia(b[2^127 + 0], k3) +
+		Camellia(b[2^127 + 1], k3) +
+		Camellia(b[2^127 + 2], k3) +
+		...
+
+	a4:
+		Camellia(b[3 * 2^126 + 0], k4) +
+		Camellia(b[3 * 2^126 + 1], k4) +
+		Camellia(b[3 * 2^126 + 2], k4) +
 		...
 
 	b:
@@ -43,19 +55,34 @@
 	s2:
 		GetCryptoSeed(, 4096, CA_SEED_FILE) // 1プロセス中1度だけ呼ばれる。
 
+	k3:
+		SHA512(s3)の前半256bit
+
+	s3:
+		GetCryptoSeed(, 4096, CA2_SEED_FILE) // 1プロセス中1度だけ呼ばれる。
+
+	k4:
+		SHA512(s4)の前半256bit
+
+	s4:
+		GetCryptoSeed(, 4096, CA3_SEED_FILE) // 1プロセス中1度だけ呼ばれる。
+
 	- - -
 
 	Camellia(plain, raw-key) = 鍵長 256 bit の Camellia ブロックの暗号化
 
-	2^128 * 16 バイトの周期を持つが、量的に一周することは有り得ないだろう。
+	2^128 * 16 バイト（最悪でも k1 == k2 == k3 == k4 のときは 2^128 * 4 バイト）の周期を持つが、
+	量的に一周することは有り得ないだろう。
 
 */
 
-#include "CryptoRand.h"
+#include "CryptoRand_v2.h"
 
 #define SEED_DIR "C:\\Factory\\tmp"
-#define SEED_FILE    SEED_DIR "\\CSeed.dat"
-#define CA_SEED_FILE SEED_DIR "\\CSeedCa.dat"
+#define SEED_FILE     SEED_DIR "\\CSeed_v2.dat"
+#define CA_SEED_FILE  SEED_DIR "\\CSeedCa_v2.dat"
+#define CA2_SEED_FILE SEED_DIR "\\CSeedCa2_v2.dat"
+#define CA3_SEED_FILE SEED_DIR "\\CSeedCa3_v2.dat"
 
 #define SEEDSIZE 4096
 
@@ -120,38 +147,35 @@ static void XorBlock(void *b1, void *b2)
 }
 static void GetCryptoBlock(uchar *buffer)
 {
-	static camellia_keyTable_t *cam_kt[2];
+	static camellia_keyTable_t *cam_kt[4];
 	static uchar counter[16];
 	static uchar dest[16];
+	uint index;
 
 	if(!cam_kt[0]) // 初期化
 	{
-		uchar (*cam_seed)[SEEDSIZE] = (uchar (*)[SEEDSIZE])memAlloc(SEEDSIZE * 2);
+		uchar (*cam_seed)[SEEDSIZE] = (uchar (*)[SEEDSIZE])memAlloc(SEEDSIZE * 4);
 		autoBlock_t gab;
 
 		GetCryptoSeed(cam_seed[0], SEEDSIZE, SEED_FILE);
 		GetCryptoSeed(cam_seed[1], SEEDSIZE, CA_SEED_FILE);
+		GetCryptoSeed(cam_seed[2], SEEDSIZE, CA2_SEED_FILE);
+		GetCryptoSeed(cam_seed[3], SEEDSIZE, CA3_SEED_FILE);
 
 		sha512_localize();
 
-		sha512_makeHashBlock(gndBlockVar(cam_seed[0], SEEDSIZE, gab));
-		memcpy(cam_seed[0], sha512_hash, 32);
-		sha512_makeHashBlock(gndBlockVar(cam_seed[1], SEEDSIZE, gab));
-		memcpy(cam_seed[1], sha512_hash, 32);
-
+		for(index = 0; index < 4; index++)
+		{
+			sha512_makeHashBlock(gndBlockVar(cam_seed[index], SEEDSIZE, gab));
+			memcpy(cam_seed[index], sha512_hash, 32);
+			cam_kt[index] = camellia_createKeyTable(gndBlockVar(cam_seed[index], 32, gab));
+		}
 		sha512_unlocalize();
-
-		errorCase(!memcmp(cam_seed[0], cam_seed[1], SEEDSIZE)); // 不幸な事故
-
-		cam_kt[0] = camellia_createKeyTable(gndBlockVar(cam_seed[0], 32, gab));
-		cam_kt[1] = camellia_createKeyTable(gndBlockVar(cam_seed[1], 32, gab));
 
 		memFree(cam_seed);
 	}
 	else // カウンタ更新
 	{
-		uint index;
-
 		for(index = 0; ; index++)
 //		for(index = 0; index < 16; index++)
 		{
@@ -164,8 +188,19 @@ static void GetCryptoBlock(uchar *buffer)
 		}
 	}
 	camellia_encrypt(cam_kt[0], counter, buffer, 1);
+	counter[15] ^= 0x40;
+
 	camellia_encrypt(cam_kt[1], counter, dest, 1);
 	XorBlock(buffer, dest);
+	counter[15] ^= 0xc0;
+
+	camellia_encrypt(cam_kt[2], counter, dest, 1);
+	XorBlock(buffer, dest);
+	counter[15] ^= 0x40;
+
+	camellia_encrypt(cam_kt[3], counter, dest, 1);
+	XorBlock(buffer, dest);
+	counter[15] ^= 0xc0;
 }
 uint getCryptoByte(void)
 {
