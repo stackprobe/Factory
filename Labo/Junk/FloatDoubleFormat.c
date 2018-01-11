30690948a94426f5411fe5f1d08c16b42eba6bcd
stackprobe <stackprobes@gmail.com>
2018-01-12 02:18:48 +0900

    daily

--- Labo/Junk/FloatDoubleFormat.c
+++ Labo/Junk/FloatDoubleFormat.c
@@ -1,7 +1,42 @@
 #include "C:\Factory\Common\all.h"
 #include "C:\Factory\Common\Options\Calc.h"
 
-static char *GetString(void *pVal, uint valSz, uint expBits, uint expBase, uint basement)
+static autoBlock_t *BinStrToBlock(char *str)
+{
+	autoBlock_t *dest = newBlock();
+	char *p;
+	uint index;
+	uint bit;
+
+	str = strx(str);
+	reverseLine(str);
+	p = str;
+
+	for(index = 0; *p; index++)
+	{
+		int chr = 0x00;
+
+		for(bit = 1; bit < 0x100; bit <<= 1)
+		{
+			switch(*p++)
+			{
+			case '0':
+				break;
+
+			case '1':
+				chr |= bit;
+				break;
+
+			default:
+				error_m(xcout("文字数は8の倍数にしてね。(%u文字)", strlen(str)));
+			}
+		}
+		addByte(dest, chr);
+	}
+	memFree(str);
+	return dest;
+}
+static void ShowFloat_Main(void *pVal, uint valSz, uint expBits, uint expBase, uint basement, char *strPrintf_x)
 {
 	char *str = strx("");
 	uint index;
@@ -17,6 +52,7 @@ static char *GetString(void *pVal, uint valSz, uint expBits, uint expBase, uint 
 	}
 	reverseLine(str);
 
+	cout("----\n");
 	cout("BINA = %s\n", str);
 
 	sign = strxl(str, 1);
@@ -52,7 +88,7 @@ static char *GetString(void *pVal, uint valSz, uint expBits, uint expBase, uint 
 					2
 					),
 				2,
-				2000
+				expBase + 30 // + margin
 				),
 			'*',
 			xcout("1.%s", fract),
@@ -70,36 +106,61 @@ static char *GetString(void *pVal, uint valSz, uint expBits, uint expBase, uint 
 	if(calcLastMarume)
 		str = calcLineToMarume_x(str, basement);
 
-	return str; // g
+	cout("DVAL = %s\n", str);
+	cout("PRNF = %s\n", strPrintf_x);
+
+	memFree(str);
+	memFree(strPrintf_x);
 }
 #if 1
 static void ShowFloat(float value)
 {
-	cout("%.10f\n", value);
-	cout("%s\n", GetString(&value, 4, 8, 127, 10));
+	ShowFloat_Main(&value, 4, 8, 127, 10, xcout("%.10f", value));
 }
 static void ShowDouble(double value)
 {
-	cout("%.20f\n", value);
-	cout("%s\n", GetString(&value, 8, 11, 1023, 20));
+	ShowFloat_Main(&value, 8, 11, 1023, 20, xcout("%.20f", value));
 }
 #else
 static void ShowFloat(float value)
 {
-	cout("%.100f\n", value);
-	cout("%s\n", GetString(&value, 4, 8, 127, 100));
+	ShowFloat_Main(&value, 4, 8, 127, 100, xcout("%.100f", value));
 }
 static void ShowDouble(double value)
 {
-	cout("%.400f\n", value);
-	cout("%s\n", GetString(&value, 8, 11, 1023, 400));
+	ShowFloat_Main(&value, 8, 11, 1023, 400, xcout("%.400f", value));
 }
 #endif
+static void ShowFloat_Block(autoBlock_t *block)
+{
+	switch(getSize(block))
+	{
+	case 4:
+		ShowFloat(*(float *)b(block));
+		break;
+
+	case 8:
+		ShowDouble(*(double *)b(block));
+		break;
+
+	default:
+		error();
+	}
+}
 int main(int argc, char **argv)
 {
+	if(argIs("/NAN"))
+	{
+		float f = powf(-1.0f, 1.5f);
+		double d = pow(-1.0, 1.5);
+
+		ShowFloat(f);
+		ShowDouble(d);
+		return;
+	}
 	if(argIs("/INF"))
 	{
-		float f  = 3.0;
+		float f  = 3.0f;
 		double d = 3.0;
 		uint c;
 
@@ -114,23 +175,28 @@ int main(int argc, char **argv)
 		ShowDouble(-d);
 		return;
 	}
+	if(argIs("/B"))
+	{
+		/*
+			最初の文字が float の最上位ビット
+			最後の文字が float の最下位ビット
+		*/
+		autoBlock_t *block = BinStrToBlock(nextArg());
+
+		ShowFloat_Block(block);
+		releaseAutoBlock(block);
+		return;
+	}
 	if(argIs("/H"))
 	{
+		/*
+			最初の文字が float の最下位バイトの上位 4 ビット
+			最後の文字が float の最上位バイトの下位 4 ビット
+		*/
 		autoBlock_t *block = makeBlockHexLine(nextArg());
 
-		switch(getSize(block))
-		{
-		case 4:
-			ShowFloat(*(float *)b(block));
-			break;
-
-		case 8:
-			ShowDouble(*(double *)b(block));
-			break;
-
-		default:
-			error();
-		}
+		ShowFloat_Block(block);
+		releaseAutoBlock(block);
 		return;
 	}
 
