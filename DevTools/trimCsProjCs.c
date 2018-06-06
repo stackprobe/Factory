d184082a14b99002ddbc8a168730b589b54d2646
stackprobe <stackprobes@gmail.com>
2018-06-06 22:38:53 +0900

    coffee break

---
+++ DevTools/trimCsProjCs.c
@@ -0,0 +1,15 @@
+/*
+	trimCsProjCs.exe [/D | ソリューションがあるディレクトリ]
+*/
+
+#include "C:\Factory\Common\all.h"
+
+int main(int argc, char **argv)
+{
+	// TODO
+
+
+	// \40\40\40\40<Compile Include="BatchServer.cs" />   --   isFairRelPath -- 配下の相対パスか
+
+
+}
