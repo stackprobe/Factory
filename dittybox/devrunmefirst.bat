ccfba6df7036474722fae561fd9836c010074043
stackprobe <stackprobes@gmail.com>
2018-08-20 22:28:52 +0900

    coffee break

--- dittybox/devrunmefirst.bat
+++ dittybox/devrunmefirst.bat
@@ -1,2 +1,2 @@
-C:\Factory\DevTools\lss . \AUTHORS* \LICENSE* .cs* .sln* .csproj* .user* .resx* .settings* .bat* .bat_* .txt* .c* .h* .csv*
+C:\Factory\DevTools\lss . \AUTHORS* \LICENSE* .cs* .sln* .csproj* .user* .resx* .settings* .bat* .bat_* .txt* .c* .h* .csv* .conf*
 C:\Factory\Tools\crlf /crlf /k /lss
