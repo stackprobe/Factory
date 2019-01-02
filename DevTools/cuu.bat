72e0ba6fdbd65a85b920fea36f6861f55f7129f8
stackprobe <stackprobes@gmail.com>
2019-01-02 12:06:40 +0900

    tea time

--- DevTools/cuu.bat
+++ DevTools/cuu.bat
@@ -1,4 +1,4 @@
-@set @dirs=C:\Factory C:\Dev C:\pleiades\workspace\Spica01\src C:\pleiades\workspace\Spica02\src C:\pleiades\workspace\Test01\src C:\pleiades\workspace\Test02\src C:\pleiades\workspace\Test03\src-03 C:\pleiades\workspace\Test04\src-04
+@set @dirs=C:\Factory C:\Dev C:\pleiades\workspace
 fndbrknuuid.exe %@dirs%
 IF ERRORLEVEL 1 PAUSE
 chkuuidcoll.exe %@dirs%
