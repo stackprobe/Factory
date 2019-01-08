c940acf2a86b88d571e95a97ad3c2f60f0902dfa
stackprobe <stackprobes@gmail.com>
2019-01-09 01:30:30 +0900

    daily

---
+++ DevTools/newcpp.c
@@ -0,0 +1,75 @@
+/*
+	newcpp g プロジェクト名
+*/
+
+#include "C:\Factory\Common\all.h"
+
+static void RenamePaths(char *fromPtn, char *toPtn)
+{
+	autoList_t *paths = ls(".");
+	char *path;
+	char *nPath;
+	uint dirCount;
+	uint index;
+
+	eraseParents(paths);
+	dirCount = lastDirCount;
+
+	foreach(paths, path, index)
+	{
+		if(index < dirCount)
+		{
+			addCwd(path);
+			{
+				RenamePaths(fromPtn, toPtn);
+			}
+			unaddCwd();
+		}
+		nPath = replaceLine(strx(path), fromPtn, toPtn, 1);
+
+		if(replaceLine_getLastReplacedCount())
+		{
+			coExecute_x(xcout("REN \"%s\" \"%s\"", path, nPath));
+		}
+		memFree(nPath);
+	}
+	releaseDim(paths, 1);
+}
+static void Main2(char *tmplProject, char *tmplDir)
+{
+	char *project = nextArg();
+
+	errorCase(!existDir(tmplDir));
+
+	errorCase_m(!lineExp("<1,30,__09AZaz>", project), "不正なプロジェクト名です。");
+	errorCase_m(existPath(project), "既に存在します。");
+
+	createDir(project);
+	copyDir(tmplDir, project);
+
+	addCwd(project);
+	{
+		coExecute("qq -f");
+
+		RenamePaths(tmplProject, project);
+
+		removeFileIfExist("C:\\Factory\\tmp\\Sections.txt"); // 意図しない検索結果を trep しないように、念のため検索結果をクリア
+
+		coExecute_x(xcout("Search.exe %s", tmplProject));
+		coExecute_x(xcout("trep.exe /F %s", project));
+
+//		coExecute("runsub _copylib");
+
+		execute("START .");
+	}
+	unaddCwd();
+}
+int main(int argc, char **argv)
+{
+	if(argIs("G"))
+	{
+		Main2("GGGGTMPL", "C:\\Dev\\Annex\\Template\\GameTemplate");
+		return;
+	}
+	cout("usage: newcpp (G｜?｜?｜?) プロジェクト名\n");
+}
--- DevTools/newcs.c
+++ DevTools/newcs.c
@@ -187,7 +187,7 @@ static void Main2(char *tmplProject, char *tmplDir, int utFlag, int m2Flag)
 		execute_x(xcout("%s.sln", project));
 
 		if(m2Flag)
-			execute("START /MAX C:\\Dev\\CSharp\\Module2\\Module2");
+			execute("START C:\\Dev\\CSharp\\Module2\\Module2");
 	}
 	unaddCwd();
 }
