9c39fbb3e8c996c8750a9de8b210f88766a0ebb6
stackprobe <stackprobes@gmail.com>
2019-03-14 20:24:04 +0900

    coffee break

--- SubTools/BlueFish/DyIPChk.c
+++ SubTools/BlueFish/DyIPChk.c
@@ -1,3 +1,7 @@
+/*
+	DyIPChk.exe チェックするドメイン名
+*/
+
 #include "C:\Factory\Common\all.h"
 
 #define SAVE_DATA_FILE "C:\\appdata\\DyIPChk_SaveData.txt"
@@ -5,7 +9,7 @@
 #define DY_IP_CHK_COMMAND "C:\\Factory\\Labo\\Socket\\hget.exe /L http://ieserver.net/ipcheck.shtml IP.tmp"
 #define IP_OUT_FILE "IP.tmp"
 
-static char *LastDyIP;
+static uint NoIPCount;
 static time_t LastUpdatedTime;
 
 static void LoadData(void)
@@ -14,14 +18,14 @@ static void LoadData(void)
 	{
 		FILE *fp = fileOpen(SAVE_DATA_FILE, "rt");
 
-		LastDyIP = neReadLine(fp);
+		NoIPCount = toValue(neReadLine(fp));
 		LastUpdatedTime = toValue64(neReadLine(fp));
 
 		fileClose(fp);
 	}
 	else
 	{
-		LastDyIP = strx("no-data");
+		NoIPCount = 0;
 		LastUpdatedTime = 0;
 	}
 }
@@ -30,25 +34,26 @@ static void SaveData(void)
 	{
 		FILE *fp = fileOpen(SAVE_DATA_FILE, "wt");
 
-		writeLine(fp, LastDyIP);
+		writeLine_x(fp, xcout("%u", NoIPCount));
 		writeLine_x(fp, xcout("%I64d", LastUpdatedTime));
 
 		fileClose(fp);
 	}
 }
-static char *GetIP(char *command)
+static char *GetIP_x(char *command)
 {
 	char *dir = makeTempDir("DyIPChk.tmp");
 	char *ip = NULL;
 
 	addCwd(dir);
 	{
-		coExecute(DY_IP_CHK_COMMAND);
+		coExecute(command);
 
 		if(existFile(IP_OUT_FILE))
 		{
-			ip = readText_b(IP_OUT_FILE);
-			ucTrim(ip);
+			ip = readFirstLine(IP_OUT_FILE);
+
+			coutJLine_x(xcout("Recved_IP.1=[%s]", ip)); // test
 
 			if(!lineExp("<1,3,09>.<1,3,09>.<1,3,09>.<1,3,09>", ip)) // ? ! IP address
 			{
@@ -60,62 +65,78 @@ static char *GetIP(char *command)
 	}
 	unaddCwd();
 
-	removeDir_x(dir);
+	removeDir(dir);
+
+	memFree(dir);
+	memFree(command);
+
+	coutJLine_x(xcout("Recved_IP.2=[%s]", ip ? ip : "<NULL>")); // test
+
 	return ip;
 }
 static char *GetDyIP(void)
 {
-	return GetIP(DY_IP_CHK_COMMAND);
+	return GetIP_x(strx(DY_IP_CHK_COMMAND));
 }
 static char *GetDomainIP(char *domain)
 {
-	return GetIP(xcout("C:\\Factory\\Labo\\Socket\\Lookup.exe %s > %s", domain, IP_OUT_FILE)); // gomi
+	return GetIP_x(xcout("C:\\Factory\\Tools\\Lookup.exe %s > %s", domain, IP_OUT_FILE));
 }
-static int CheckDomainIP(void)
-{
-	return 1; // TODO
-}
-int main(int argc, char **argv)
+static void CheckDyIP(char *domain)
 {
 	uint retCode = 0;
 	char *dyIP;
+	char *domainIP;
 	time_t currTime = time(NULL);
+
+	cout("domain: %s\n", domain);
 
 	LoadData();
 
-	cout("L.LastDyIP: %s\n", LastDyIP);
+	cout("L.NoIPCount: %u\n", NoIPCount);
 	cout("L.LastUpdatedTime: %I64d\n", LastUpdatedTime);
 
 	dyIP = GetDyIP();
+	domainIP = GetDomainIP(domain);
 
-	if(dyIP)
+	if(dyIP && domainIP)
 	{
-		cout("dyIP: %s\n", dyIP);
-
-		if(strcmp(LastDyIP, dyIP))
+		if(strcmp(dyIP, domainIP))
 		{
-			memFree(LastDyIP);
-			LastDyIP = dyIP;
-
+			cout("IP不一致\n");
 			retCode = 1;
 		}
-		else
-			memFree(dyIP);
+		NoIPCount = 0;
 	}
-	if(CheckDomainIP())
+	else
+		NoIPCount++;
+
+	if(10 < NoIPCount)
 		retCode = 1;
 
 	if(LastUpdatedTime + 35 * 86400 < currTime)
 		retCode = 1;
 
 	if(retCode == 1)
+	{
+		NoIPCount = 0;
 		LastUpdatedTime = currTime;
-
-	cout("S.LastDyIP: %s\n", LastDyIP);
+	}
+	cout("S.NoIPCount: %u\n", NoIPCount);
 	cout("S.LastUpdatedTime: %I64d\n", LastUpdatedTime);
 
 	SaveData();
 
+	memFree(dyIP);
+	memFree(domainIP);
 	cout("retCode: %u\n", retCode);
-	return retCode;
+	termination(retCode);
+}
+int main(int argc, char **argv)
+{
+	char *domain = nextArg();
+
+	errorCase_m(!lineExp("<1,300,-.09AZaz>", domain), "不正なドメイン名");
+
+	CheckDyIP(domain);
 }
