a6f04616d2f4e55f08d0e16776784237b46ebfb0
stackprobe <stackprobes@gmail.com>
2019-09-05 21:26:12 +0900

    Amour

--- Tools/Drop2Text.c
+++ Tools/Drop2Text.c
@@ -1,8 +1,21 @@
+/*
+	Drop2Text.exe [/F]
+
+		/F ... フォルダがドロップされた場合、その配下のファイルを追加する。
+*/
+
 #include "C:\Factory\Common\all.h"
+
+static int FileOnlyMode;
 
 int main(int argc, char **argv)
 {
 	autoList_t *paths = newList();
+
+	if(argIs("/F"))
+	{
+		FileOnlyMode = 1;
+	}
 
 	for(; ; )
 	{
@@ -11,7 +24,14 @@ int main(int argc, char **argv)
 		if(!path)
 			break;
 
-		addElement(paths, (uint)path);
+		if(FileOnlyMode && existDir(path))
+		{
+			autoList_t *files = lssFiles(path);
+			sortJLinesICase(files);
+			addElements_x(paths, files);
+		}
+		else
+			addElement(paths, (uint)path);
 	}
 	writeLines_cx("C:\\Factory\\tmp\\Drop2Text_Last.txt", editLines_x(paths));
 }
