b0eacdff966a8b17232ddebb54c6fb22b5d7df6d
stackprobe <stackprobes@gmail.com>
2020-02-21 02:55:50 +0900

    Nile

--- DevTools/lssColl.c
+++ DevTools/lssColl.c
@@ -1,6 +1,27 @@
 #include "C:\Factory\Common\all.h"
 
-int main(int argc, char **argv)
+static void CollectKeepTree(void)
+{
+	autoList_t *paths = readLines(FOUNDLISTFILE);
+	char *path;
+	uint index;
+	char *outDir = makeFreeDir();
+
+	foreach(paths, path, index)
+	{
+		char *outPath = combine_cx(outDir, lineToFairRelPath(path, strlen(outDir)));
+
+		cout("< %s\n", path);
+		cout("> %s\n", outPath);
+
+		createPath(outPath, 'X');
+		copyPath(path, outPath);
+		memFree(outPath);
+	}
+	execute_x(xcout("START %s", outDir));
+	memFree(outDir);
+}
+static void Collect(void)
 {
 	autoList_t *paths = readLines(FOUNDLISTFILE);
 	char *path;
@@ -19,4 +40,15 @@ int main(int argc, char **argv)
 	}
 	execute_x(xcout("START %s", outDir));
 	memFree(outDir);
+}
+int main(int argc, char **argv)
+{
+	if(argIs("/K"))
+	{
+		CollectKeepTree();
+	}
+	else
+	{
+		Collect();
+	}
 }
