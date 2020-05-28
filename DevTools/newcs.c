dc30de12015281299ab8101816300171f9265e83
stackprobe <stackprobes@gmail.com>
2020-05-29 03:30:11 +0900

    Fontaine

--- DevTools/newcs.c
+++ DevTools/newcs.c
@@ -205,7 +205,8 @@ static void Main2(char *tmplProject, char *tmplDir, int utFlag, int m2Flag, char
 		execute_x(xcout("%s.sln", project));
 
 		if(m2Flag)
-			execute("START C:\\Dev\\CSharp\\Module2\\Module2");
+			execute("START C:\\Dev\\CSharp\\Module2");
+//			execute("START C:\\Dev\\CSharp\\Module2\\Module2"); // del @ 2020.5.23
 	}
 	unaddCwd();
 }
